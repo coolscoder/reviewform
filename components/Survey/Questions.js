0dda5334b3ac094056b387a84b5f60fae4e3c6ea
Koichi Noda <koichi.p.noda@gmail.com>
2022-01-11 21:30:30 +0900

    feat: clean up code bases

--- components/Survey/Questions.js
+++ components/Survey/Questions.js
@@ -5,13 +5,13 @@ import FormControlLabel from '@mui/material/FormControlLabel'
 import styles from './styles.module.css'
 
 const Questions = ({ label='' }) => (
-  <div>
+  <>
     <p className={styles.label}>{label}</p>
     <RadioGroup row>
         <FormControlLabel value="yes" control={<Radio />} label="Yes" />
         <FormControlLabel value="no" control={<Radio />} label="No" />
       </RadioGroup>
-  </div>
+  </>
 )
 
 export default Questions
