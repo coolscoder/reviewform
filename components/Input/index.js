260395b4647c19343e69dbfaef60de8aa25634ac
Koichi Noda <koichi.p.noda@gmail.com>
2022-01-11 17:00:50 +0900

    feat: add global input component

---
+++ components/Input/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TextField from '@mui/material/TextField'
+import styles from './styles.module.css'
+
+const Input = ({ label, value, onChange }) => (
+  <>
+    <p className={styles.label}>{label}</p>
+    <TextField
+      value={value}
+      onChange={onChange}
+    />
+  </>
+)
+
+export default Input
