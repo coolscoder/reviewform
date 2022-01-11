aeede8101f86fd1c78a808c02cda35a5fca03409
Koichi Noda <koichi.p.noda@gmail.com>
2022-01-11 17:44:54 +0900

    feat: add personal details

--- pages/PersonalDetails/index.js
+++ pages/PersonalDetails/index.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../../components/Title'
+import Input from '../../components/Input'
 import styles from './styles.module.css'
 
 const PersonalDetails = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
   return (
-    <Title title="Personal Details" />
+    <div className={styles.personal}>
+      <Title title="Personal Details" />
+      <Input
+        label="1. Your Name"
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <Input
+        label="2. Your Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+    </div>
   )
 }
 
