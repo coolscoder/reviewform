f517c9617d8b58ecf2e9c67f065e832603c71366
Koichi Noda <koichi.p.noda@gmail.com>
2022-01-11 17:41:18 +0900

    feat: add transacted

--- components/Input/index.js
+++ components/Input/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import TextField from '@mui/material/TextField'
 import styles from './styles.module.css'
 
-const Input = ({ label, value, onChange }) => (
+const Input = ({ label = '', value, onChange }) => (
   <>
     <p className={styles.label}>{label}</p>
     <TextField
--- pages/Transacted/index.js
+++ pages/Transacted/index.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../../components/Title'
+import Input from '../../components/Input'
 import styles from './styles.module.css'
 
 const Transacted = () => {
+  const [type, setType] = useState('HBD')
+  const [address1, setAddress1] = useState('Ave 1 Robinson ')
+  const [address2, setAddress2] = useState('Blk 123, S(000100)')
+  const [unit, setUnit] = useState(0)
+
   return (
-    <Title title="Transacted Property Details" />
+    <div className={styles.transacted}>
+      <Title title="Transacted Property Details" />
+      <Input
+        label="1. Property Type"
+        value={type}
+        onChange={e => setType(e.target.value)}
+      />
+      <Input
+        label="1. Property Type"
+        value={address1}
+        onChange={e => setAddress1(e.target.value)}
+      />
+      <Input
+        value={address2}
+        onChange={e => setAddress2(e.target.value)}
+      />
+      <Input
+        label="3. Unit Number (Optional)"
+        value={unit}
+        onChange={e => setUnit(e.target.value)}
+      />
+    </div>
   )
 }
 
